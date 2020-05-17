33417acc08f1835950ea382079978676a3f5b6e5
rolanorebelo <rolano.rebelo@gmail.com>
2020-05-17 11:15:14 +0530

    added buy/sell feature

---
+++ client/app.js
@@ -0,0 +1,23 @@
+
+var socket = require("socket.io-client")("http://localhost:8000");
+// var gpio = require("rpi-gpio");
+
+// process.on("SIGINT", function(){
+//   gpio.write(config.led, 1, function(){
+//     gpio.destroy(function(){
+//       process.exit();
+//     });
+//   });
+// });
+
+// gpio.setup(config.led, gpio.DIR_OUT, function(){
+//   gpio.write(config.led, 1); // turns led off
+// });
+
+socket.on("connect", function(){
+  console.log("Connected to server");
+  socket.on("updateState", function(state){
+    console.log("The new state is: " + state);
+    // gpio.write(config.led, !state);
+  });
+})
